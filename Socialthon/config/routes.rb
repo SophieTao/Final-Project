ae3361dcca04cd6f8f169ead7643f4b84e325d84
Yining <sophietaoyn@gmail.com>
2015-06-08 17:57:00 +0200

    before projects

--- Socialthon/config/routes.rb
+++ Socialthon/config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 resources :users
 resources :communities
 resources :teams
+resources :projects
   # You can have the root of your site routed with "root"
    root 'users#index'
 
