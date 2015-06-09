83f1925fedbcb24decdcc675f3b39cbf55186f2e
BigYuns <sungyun.chung94@gmail.com>
2015-06-09 20:43:30 +0200

    Yunny: Everything works

--- Socialthon/db/schema.rb
+++ Socialthon/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150608202942) do
+ActiveRecord::Schema.define(version: 20150609143836) do
 
   create_table "arts", force: :cascade do |t|
     t.datetime "created_at", null: false
